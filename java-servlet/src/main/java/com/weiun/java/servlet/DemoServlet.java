7eae408fcfcb562dbba3b797361c9ee73de266e4
william <william.wei@g-town.com.cn>
2019-02-21 11:29:04 +0800

    Servlet

---
+++ java-servlet/src/main/java/com/weiun/java/servlet/DemoServlet.java
@@ -0,0 +1,38 @@
+package com.weiun.java.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author William
+ * @Date 2019/2/21
+ * @Description Java Servlet
+ */
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}

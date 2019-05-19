f906b0e62d81c0cde2ea2105c5d181abd08464e5
weiun <weijj0528@163.com>
2019-05-19 12:26:28 +0800

    SpringBoot返回xml格式数据

--- springboot-base/src/main/java/com/weiun/springboot/base/controller/RestDemoController.java
+++ springboot-base/src/main/java/com/weiun/springboot/base/controller/RestDemoController.java
@@ -1,5 +1,7 @@
 package com.weiun.springboot.base.controller;
 
+import com.weiun.springboot.base.bean.Response;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,16 @@ public class RestDemoController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
+    }
+
+    @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response jsonResponse() {
+        return new Response();
+    }
+
+    @GetMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public Response xmlResponse() {
+        return new Response();
     }
 
 }

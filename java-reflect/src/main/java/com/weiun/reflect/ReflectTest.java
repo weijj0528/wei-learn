5486ec286e667564d58365d6bdc2f5ef752510b5
weijj0528 <weijj0528@163.com>
2019-07-24 09:46:49 +0800

    反射其他相关

--- java-reflect/src/main/java/com/weiun/reflect/ReflectTest.java
+++ java-reflect/src/main/java/com/weiun/reflect/ReflectTest.java
@@ -6,22 +6,26 @@ import java.lang.reflect.Type;
 /**
  * @author William
  * @Date 2019/3/19
- * @Description https://mp.weixin.qq.com/s?src=11&timestamp=1553052851&ver=1495&signature=EE1DKExrbNksPgto-khqr*bmUCZKGV-Tcrvoy93fX5arcWPaPavYwLBnc6RfDa750FRrCLdWlebktbpwht0tr4IIDPz1siM3OoxRGVv5-r5kavxY193YJ31*6-GP4GDh&new=1
+ * @Description 反射其他
  */
 public class ReflectTest {
 
 
     public static void main(String[] args) throws Exception {
+        // 获取类
         Class<?> clazz = Class.forName("com.weiun.reflect.bean.User");
-        /**获取父类相关*/
         // 获取父类型(同接口)
         Class<?> superclass = clazz.getSuperclass();
-        // 获取带参数的父类型(同接口)
+        // 获取接口类型
+        Class<?>[] interfaces = clazz.getInterfaces();
+        // 获取带参数的父类型 获取泛型类型
         Type genericSuperclass = clazz.getGenericSuperclass();
         ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
         Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
         System.out.println(actualTypeArguments[0]);
         System.out.println(genericSuperclass);
+        // 获取带参数的接口 获取泛型类型
+        Type[] genericInterfaces = clazz.getGenericInterfaces();
     }
 
 

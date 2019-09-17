c148a4ea6585a5c770d273fcfe07aab2adf900a0
William.Wei <William.Wei@g-town.com.cn>
2019-09-17 08:59:17 +0800

    测试

--- java-base/src/main/java/com/weiun/base/BaseTest.java
+++ java-base/src/main/java/com/weiun/base/BaseTest.java
@@ -6,6 +6,9 @@ public class BaseTest {
     public static void main(String[] args) {
         System.out.println(addAfter(1));
         System.out.println(addBefore(1));
+
+        String format = String.format("%01d", 2);
+        System.out.println(format);
     }
 
     private static int addAfter(int i) {

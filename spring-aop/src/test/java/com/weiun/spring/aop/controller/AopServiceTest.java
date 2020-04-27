e3c2fc2abf1ad7761c0ffb56dd17893c5d2d500c
William.Wei <William.Wei@g-town.com.cn>
2020-04-27 17:05:32 +0800

    Spring aop两种配置方式

--- spring-aop/src/test/java/com/weiun/spring/aop/controller/AopServiceTest.java
+++ spring-aop/src/test/java/com/weiun/spring/aop/controller/AopServiceTest.java
@@ -1,22 +1,29 @@
 package com.weiun.spring.aop.controller;
 
+import com.weiun.spring.aop.service.OneService;
+import com.weiun.spring.aop.service.SecondService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
-public class DemoControllerTest {
+public class AopServiceTest {
 
     @Autowired
-    private DemoController demoController;
+    private OneService oneService;
+    @Autowired
+    private SecondService secondService;
 
     @Test
-    public void test1() {
-        Object test = demoController.test();
-        System.out.println(test);
+    public void oneTest() {
+        oneService.aop();
+    }
+
+    @Test
+    public void secondTest() {
+        secondService.aop();
     }
 }

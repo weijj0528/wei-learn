3c9b37f4882d3d5dad6d6ec355b9f019790ce264
William.Wei <William.Wei@g-town.com.cn>
2020-08-31 18:18:17 +0800

    Spring声明式事务

--- spring/spring-tx/src/main/java/com/weiun/springtx/AcountService.java
+++
@@ -1,4 +0,0 @@
-package com.weiun.springtx;
-
-public interface AcountService {
-}
---
+++ spring/spring-tx/src/test/java/com/weiun/springtx/jdbc/AccountServiceJdbcTemplateTest.java
@@ -0,0 +1,30 @@
+package com.weiun.springtx.jdbc;
+
+import com.weiun.springtx.jdbc.service.AcountService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AccountServiceJdbcTemplateTest {
+
+
+    private ApplicationContext context;
+
+    @Before
+    public void before() {
+        context = new ClassPathXmlApplicationContext("applicationContext-jdbc.xml");
+    }
+
+    @Test
+    public void transferSuccess() throws Exception {
+        AcountService acountService = (AcountService) context.getBean("accountServiceJdbcTemplateImpl");
+        acountService.transferSuccess(1, 0, 10);
+    }
+
+    @Test
+    public void transferError() throws Exception {
+        AcountService acountService = (AcountService) context.getBean("accountServiceJdbcTemplateImpl");
+        acountService.transferError(1, 0, 10);
+    }
+}

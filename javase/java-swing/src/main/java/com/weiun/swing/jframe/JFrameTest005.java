6a25fca2eacefe2eae90093a007676c83b6aa9b9
William.Wei <William.Wei@g-town.com.cn>
2020-09-25 15:03:44 +0800

    Swing

--- javase/java-swing/src/main/java/com/weiun/swing/jframe/JFrameTest005.java
+++ javase/java-swing/src/main/java/com/weiun/swing/jframe/JFrameTest005.java
@@ -3,7 +3,10 @@ package com.weiun.swing.jframe;
 import javax.swing.*;
 import java.awt.*;
 
-public class GraphicsTest {
+/**
+ * 画图演示
+ */
+public class JFrameTest005 {
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("绘图测试");
@@ -16,21 +19,21 @@ public class GraphicsTest {
                 g.drawArc(0, 0, 100, 100, 0, 90);
                 // 直线
                 g.setColor(Color.YELLOW);
-                g.drawLine(0,0,100,100);
+                g.drawLine(0, 0, 100, 100);
                 // 椭圆
                 g.setColor(Color.BLUE);
-                g.drawOval(100,100,200,100);
+                g.drawOval(100, 100, 200, 100);
                 // 矩形
                 g.setColor(Color.BLACK);
                 //g.drawRect(100,100,200,100);
-                g.draw3DRect(100,100,200,100,false);
+                g.draw3DRect(100, 100, 200, 100, false);
                 // 圆角矩形
                 g.setColor(Color.CYAN);
-                g.drawRoundRect(90,90,220,120,20,20);
+                g.drawRoundRect(90, 90, 220, 120, 20, 20);
                 // String
                 g.setColor(Color.GREEN);
                 //g.setColor(new Color(255,255,255));
-                g.drawString("Hello",200,150);
+                g.drawString("Hello", 200, 150);
             }
         };
         comp.setSize(400, 300);

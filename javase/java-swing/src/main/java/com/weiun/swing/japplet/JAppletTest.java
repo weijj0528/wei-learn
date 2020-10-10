3f8893d7054310441a4e26f046121a2cd6dcc2b6
William.Wei <William.Wei@g-town.com.cn>
2020-10-10 15:21:28 +0800

    调整

--- javase/java-swing/src/main/java/com/weiun/swing/japplet/JAppletTest.java
+++
@@ -1,8 +0,0 @@
-package com.weiun.swing.japplet;
-
-public class JAppletTest {
-
-    public static void main(String[] args) {
-        // 创建小应用时使用。包含在浏览器中
-    }
-}
--- javase/java-swing/src/main/java/com/weiun/swing/jdialog/JDialogTest.java
+++
@@ -1,9 +0,0 @@
-package com.weiun.swing.jdialog;
-
-public class JDialogTest {
-
-    public static void main(String[] args) {
-        // 创建对话框时使用
-    }
-
-}
